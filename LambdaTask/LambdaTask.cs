7c122eb380af626cf35b0f31a89cdc4b70c1a835
AlexRovan <rovan3014@mail.ru>
2020-12-13 21:30:41 +0700

    Исправления

--- LambdaTask/LambdaTask.cs
+++ LambdaTask/LambdaTask.cs
@@ -8,33 +8,39 @@ namespace LambdaTask
     {
         static void Main(string[] args)
         {
-            List<Person> personsList = new List<Person>()
+            var personsList = new List<Person>
             {
-                                                new Person("Иванов Иван", 33),
-                                                new Person("Николай Фомин", 35),
-                                                new Person("Золоторёв Александр", 17),
-                                                new Person("Анатолий Суслов", 73),
-                                                new Person("Николай Устинов", 22),
-                                                new Person("Анатолий Сахаров", 40),
-                                                new Person("Иванов Иван", 12),
+                new Person("Иванов Иван", 33),
+                new Person("Николай Фомин", 35),
+                new Person("Золоторёв Александр", 17),
+                new Person("Анатолий Суслов", 73),
+                new Person("Николай Устинов", 22),
+                new Person("Анатолий Сахаров", 40),
+                new Person("Иванов Иван", 12)
             };
 
-            var uniqueNamesList = personsList.Select(p => p.Name).Distinct().ToList();
-            Console.WriteLine($"Имена: {string.Join(",", uniqueNamesList)}");
+            var uniqueNamesList = personsList
+                .Select(p => p.Name)
+                .Distinct()
+                .ToList();
+            Console.WriteLine($"Имена: {string.Join(", ", uniqueNamesList)}");
 
-            var averageAgePersonsUnder18 = personsList.Where(p => p.Age < 18).Average(p => p.Age);
-            Console.WriteLine($"Средний возраст: {averageAgePersonsUnder18}");
+            var personsWithAverageAgeUnder18 = personsList
+                .Where(p => p.Age < 18)
+                .Average(p => p.Age);
+            Console.WriteLine($"Средний возраст: {personsWithAverageAgeUnder18}");
 
-            var uniquePersonWithAverageAge = personsList
+            var averageAgeByName = personsList
                 .GroupBy(p => p.Name)
                 .ToDictionary(p => p.Key, p => p.Average(p => p.Age));
+            Console.WriteLine($"Уникальные имена и их средний возраст: {string.Join(", ", averageAgeByName)}");
 
-            var sortedNameByAge = personsList
-                .Where(p => (p.Age >= 20 && p.Age <= 40))
+            var sortedNamesByAge = personsList
+                .Where(p => (p.Age >= 20 && p.Age <= 45))
                 .OrderByDescending(p => p.Age)
                 .Select(p => p.Name).ToList();
 
-            Console.WriteLine($"Имена: {string.Join(",", sortedNameByAge)}");
+            Console.WriteLine($"Имена: {string.Join(", ", sortedNamesByAge)}");
         }
     }
 }
