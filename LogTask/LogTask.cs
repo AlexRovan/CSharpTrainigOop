ed748ff2be2c13bd97c20a501b131459206e6fe7
AlexRovan <rovan3014@mail.ru>
2021-02-13 21:33:37 +0700

    Задание LogTask

---
+++ LogTask/LogTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NLog;
+
+namespace LogTask
+{
+    class LogTask
+    {
+        static void Main()
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            logger.Debug("debug message");
+            logger.Info("info message");
+
+            try
+            {
+                throw new Exception("Exception");
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+            }
+        }
+    }
+}
